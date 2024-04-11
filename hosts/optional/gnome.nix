4e3b52f45a8bd69f16b610ef90a62e7f1f282d63
Shad Kaske <shadkaske@gmail.com>
2024-04-11 13:50:45 -0500

    Streamlining gnome install

--- hosts/optional/gnome.nix
+++ hosts/optional/gnome.nix
@@ -2,6 +2,25 @@
   services.xserver.enable = true;
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
+
+  environment.gnome.excludePackages = (with pkgs; [
+    gnome-photos
+    gnome-tour
+    gnome.cheese
+    gnome.geary
+  ]) ++ (with pkgs.gnome; [
+    gnome-music
+    gnome-terminal
+    gedit
+    evince
+    gnome-characters
+    totem
+  ]);
+
+  environment.systemPackages = with pkgs; [
+    papirus-icon-theme
+    gnome.adwaita-icon-theme
+  ];
 }
 
 
