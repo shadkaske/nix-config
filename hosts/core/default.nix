882dff6d3938633692933efde5ab144f34384f23
Shad Kaske <shadkaske@gmail.com>
2024-04-11 10:36:49 -0500

    Reordering core host imports

--- hosts/core/default.nix
+++ hosts/core/default.nix
@@ -1,11 +1,11 @@
 { pkgs, config, ... }: {
   imports = [
-    ./services/openssh.nix
-    ./services/docker.nix
-    ./services/libvirt.nix
     ./settings/locale.nix
     ./settings/editor.nix
     ./settings/utilities.nix
+    ./services/openssh.nix
+    ./services/docker.nix
+    ./services/libvirt.nix
     ./settings/user.nix
     # TODO: firewall
   ];
