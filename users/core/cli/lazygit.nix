353c893e88458ed185508fef549016c1b4cb7351
Shad Kaske <shadkaske@gmail.com>
2024-04-11 10:37:06 -0500

    Lazy Git

---
+++ users/core/cli/lazygit.nix
@@ -0,0 +1,24 @@
+{ inputs, outputs, lib, config, pkgs, ... }: {
+  programs.lazygit = {
+    enable = true;
+    settings = {
+      gui = {
+        showRandomTip = false;
+        theme = {
+          activeBorderColor = [ "#89b4fa" "bold" ];
+          inactiveBorderColor = [ "#a6adc8" ];
+          optionsTextColor = [ "#89b4fa" ];
+          selectedLineBgColor = [ "#313244" ];
+          cherryPickedCommitBgColor = [ "#45475a" ];
+          cherryPickedCommitFgColor = [ "#89b4fa" ];
+          unstagedChangesColor = [ "#f38ba8" ];
+          defaultFgColor = [ "#cdd6f4" ];
+          searchingActiveBorderColor = [ "#f9e2af" ];
+        };
+      };
+    };
+  };
+}
+# vim: ts=2 sw=2 et
+
+
