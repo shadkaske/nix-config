13260b79eae20a28a404bd39db3ebfcf1e7de964
Shad Kaske <shadkaske@gmail.com>
2024-04-11 14:15:06 -0500

    Added the gnome mutter overlay
    Jammed an overlay in here from the wiki. No idea if this is better but I wanted to see what it does

--- overlays/default.nix
+++ overlays/default.nix
@@ -7,9 +7,16 @@
   # You can change versions, add patches, set compilation flags, anything really.
   # https://nixos.wiki/wiki/Overlays
   modifications = final: prev: {
-    # example = prev.example.overrideAttrs (oldAttrs: rec {
-    # ...
-    # });
+      gnome = prev.gnome.overrideScope' (gnomeFinal: gnomePrev: {
+        mutter = gnomePrev.mutter.overrideAttrs ( old: {
+          src = prev.fetchgit {
+            url = "https://gitlab.gnome.org/vanvugt/mutter.git";
+            # GNOME 45: triple-buffering-v4-45
+            rev = "0b896518b2028d9c4d6ea44806d093fd33793689";
+            sha256 = "sha256-mzNy5GPlB2qkI2KEAErJQzO//uo8yO0kPQUwvGDwR4w=";
+          };
+        } );
+      });
   };
 
   # When applied, the unstable nixpkgs set (declared in the flake inputs) will
