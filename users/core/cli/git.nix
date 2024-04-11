b519f0acac66d4eaa098e4c75b79c3deebf6d19b
Shad Kaske <shadkaske@gmail.com>
2024-04-11 10:37:02 -0500

    Added basic git config

---
+++ users/core/cli/git.nix
@@ -0,0 +1,10 @@
+{ inputs, outputs, lib, config, pkgs, ... }: {
+  programs.git = {
+    enable = true;
+    lfs.enable = true;
+    userEmail = "shadkaske@gmail.com";
+    userName = "Shad Kaske";
+  };
+}
+# vim: ts=2 sw=2 et
+
